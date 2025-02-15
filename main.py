588d394c2849961fc8f3592571ef5b15899907f4
hostintheshell <blughost.cheese@proton.me>
2025-02-14 18:32:06 -0800

    added player object

--- main.py
+++ main.py
@@ -3,18 +3,28 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
+        # Makes the exit button work
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # Draws and refreshes the screen
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # Limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
